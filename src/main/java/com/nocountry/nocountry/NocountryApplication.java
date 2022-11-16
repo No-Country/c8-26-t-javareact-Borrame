448491f32ba70d2d8ee7d4493b4608dc806b2c28
Andres Orozco <juanxxi2015@gmail.com>
2022-11-16 12:38:58 -0500

    Spring initializer done

---
+++ src/main/java/com/nocountry/nocountry/NocountryApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.nocountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NocountryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NocountryApplication.class, args);
+	}
+
+}
